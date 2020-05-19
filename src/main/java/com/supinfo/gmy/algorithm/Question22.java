93571c71a1c5fded8827815a6cef19b451652a3d
gmyLover <141297@supinfo.com>
2020-05-20 00:53:42 +0800

    1

--- src/main/java/com/supinfo/gmy/algorithm/Question22.java
+++ src/main/java/com/supinfo/gmy/algorithm/Question22.java
@@ -1,5 +1,38 @@
 package com.supinfo.gmy.algorithm;
 
-public class Question22 {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.commons.lang.ArrayUtils;
+
+/**
+ * @Description: 调整数组顺序使奇数位于偶数前面
+ * @author gmylover
+ * @date 2020-05-20
+ */
+public class Question22 {
+    public static void reOrderArray(int [] array) { 
+    	int[] list=new int[array.length];
+    	int num=0;
+    	for (int var : array) {
+			if (var%2==1) {
+				list[num]=var;
+				num++;
+			}
+		}
+    	for (int var : array) {
+			if (var%2==0) {
+				list[num]=var;
+				num++;			
+				}
+		}
+    	array=list;
+    	System.out.println(ArrayUtils.toString(array));
+    }
+    public static void main(String[] args) {
+		int[]haha={1,2,3,4,5,6,7}; 
+		reOrderArray(haha);
+	}
+    
 }
---
+++ src/main/java/com/supinfo/gmy/algorithm/Question23.java
@@ -0,0 +1,24 @@
+package com.supinfo.gmy.algorithm;
+
+/**
+ * @Description: 链表中倒数第 K 个结点
+ * @author gmylover
+ * @date 2020-05-20
+ */
+public class Question23 {
+	/* 输入一个链表，输出该链表中倒数第k个结点。 */
+	public ListNode FindKthToTail(ListNode head, int k) {
+
+		return head;
+
+	}
+
+	public class ListNode {
+		int val;
+		ListNode next = null;
+
+		ListNode(int val) {
+			this.val = val;
+		}
+	}
+}
