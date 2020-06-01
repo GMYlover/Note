7e254a33d3882dc84c98e486ad143b557585ef92
gmylover <141297@supinfo.com>
2020-06-01 15:49:18 +0800

    树的镜像

--- src/main/java/com/supinfo/gmy/algorithm/Question27.java
+++ src/main/java/com/supinfo/gmy/algorithm/Question27.java
@@ -1,22 +1,45 @@
 package com.supinfo.gmy.algorithm;
 
-import javax.swing.tree.TreeNode;
-
 /*操作给定的二叉树，将其变换为源二叉树的镜像。*/
 public class Question27 {
 	/*	二叉树的镜像定义：源二叉树 
-	8
+	     8
 	   /  \
 	  6   10
 	 / \  / \
 	5  7 9 11
 	镜像二叉树
-	8
+	     8						
 	   /  \
 	  10   6
 	 / \  / \
-	11 9 7  5*/
-	public void Mirror(TreeNode root) {
-		
+	11 9 7  5
+	{8,6,10,5,7,9,11}
+	8,10,6,11,9,7,5
+	*/
+
+	public class Solution {
+		public void Mirror(TreeNode root) {
+			if (root != null) {
+				Mirror(root.left);
+				Mirror(root.right);
+				TreeNode temp = root.left;
+				root.left = root.right;
+				root.right = temp;
+			}
+		}
 	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(8);
+		root.left = new TreeNode(6);
+		root.right = new TreeNode(10);
+		root.left.left = new TreeNode(5);
+		root.left.right = new TreeNode(7);
+		root.right.left = new TreeNode(9);
+		root.right.right = new TreeNode(11);
+
+		System.out.println("123");
+	}
+
 }
