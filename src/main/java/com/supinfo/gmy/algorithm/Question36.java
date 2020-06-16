af5522a32c36daeac7db83cfc509b391448e4654
gmylover <141297@supinfo.com>
2020-06-16 10:52:37 +0800

    提交

---
+++ src/main/java/com/supinfo/gmy/algorithm/Question36.java
@@ -0,0 +1,36 @@
+package com.supinfo.gmy.algorithm;
+
+/** 
+* @Description: 二叉搜索树的后序遍历序列
+* @author: gaomingyang
+* @date: 2020-06-12
+*/
+public class Question36 {
+	// 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+
+	public class Solution {
+
+		public boolean helpVerify(int[] sequence, int start, int root) {
+			if (start >= root)
+				return true;
+			int key = sequence[root];
+			int i;
+			// 找到左右子数的分界点
+			for (i = start; i < root; i++)
+				if (sequence[i] > key)
+					break;
+			// 在右子树中判断是否含有小于root的值，如果有返回false
+			for (int j = i; j < root; j++)
+				if (sequence[j] < key)
+					return false;
+			return helpVerify(sequence, start, i - 1) && helpVerify(sequence, i, root - 1);
+		}
+
+		public boolean VerifySquenceOfBST(int[] sequence) {
+			if (sequence == null || sequence.length == 0)
+				return false;
+			return helpVerify(sequence, 0, sequence.length - 1);
+
+		}
+	}
+}
